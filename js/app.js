56a466f7dcf98c0faa303cde12446e1266b9f288
Piotr Kabaciński <Piotr Kabaciski>
2017-08-23 10:42:14 +0200

    code review

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// Brak IIFE
+// Proponuję wydzilić kod odpowiedzialny za login do oddzielnego pliku.
+// Wszystkie zdarzenia na elementach powinny byc zawarte w funkcji
+
 $(document).foundation();
 
 
@@ -22,11 +26,15 @@ function call() {
         },
         url: "https://efigence-camp.herokuapp.com/api/login",
         error: function(response) {
+            // Nigdy nie stosujemy jednoliterowych nazw zmiennych. Od tego jest uglifier.
+            // Czemu uzywasz jQuery, skoro mozna uzyc natywnego parsera? JSON.parse().
+            // Polecam tez: http://youmightnotneedjquery.com/
             var r = jQuery.parseJSON(response.responseText);
             $('.wrongCredentials').html(r.message);
             $('.wrongCredentials').removeClass("hide");
         },
         success: function(response) {
+            // Czy tutaj nie powinno byc takze usuwania komunikatu bledu?
             window.location.href = "dashboard.html";
         }
     });
@@ -38,7 +46,4 @@ $('#cancel').click(function(e) {
     $("#user-active").addClass("hide");
 });
 
-
-
-
-
+// Po co tyle nowych linii na dole? :)
