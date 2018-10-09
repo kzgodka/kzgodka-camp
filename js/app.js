b0e93e5fa87b4835c083adeb870eb805b79f2138
kzgodka <kamilzgodka@gmail.com>
2018-10-09 12:52:57 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-// Brak IIFE
-// Proponuję wydzilić kod odpowiedzialny za login do oddzielnego pliku.
-// Wszystkie zdarzenia na elementach powinny byc zawarte w funkcji
-
 $(document).foundation();
 
 
@@ -26,15 +22,11 @@ function call() {
         },
         url: "https://efigence-camp.herokuapp.com/api/login",
         error: function(response) {
-            // Nigdy nie stosujemy jednoliterowych nazw zmiennych. Od tego jest uglifier.
-            // Czemu uzywasz jQuery, skoro mozna uzyc natywnego parsera? JSON.parse().
-            // Polecam tez: http://youmightnotneedjquery.com/
             var r = jQuery.parseJSON(response.responseText);
             $('.wrongCredentials').html(r.message);
             $('.wrongCredentials').removeClass("hide");
         },
         success: function(response) {
-            // Czy tutaj nie powinno byc takze usuwania komunikatu bledu?
             window.location.href = "dashboard.html";
         }
     });
@@ -46,4 +38,7 @@ $('#cancel').click(function(e) {
     $("#user-active").addClass("hide");
 });
 
-// Po co tyle nowych linii na dole? :)
+
+
+
+
